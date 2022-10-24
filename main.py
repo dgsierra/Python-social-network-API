9353d167b409d3afc7e1e787b71aade621096266
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-10-23 19:20:03 -0500

    Add API post function

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to my API2"}
+
+
+@app.get("/posts/")
+def get_posts():
+    return {"data": "This is a post"}
+
+@app.post("/create/")
+def create_post(payLoad: dict = Body(...)):
+    print(payLoad)
+    return {"new_post": f"Title: {payLoad['title']}, Content: {payLoad['content']}"}
